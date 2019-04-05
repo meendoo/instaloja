a4769fa873be64f8596233f319d595e71780780d
Helder Santos <contact@meendoo.com>
2019-04-04 23:03:19 -0300

    About.js animations refactored

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,53 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import styles from './about.module.scss';
-import Container from '../layout/container'
-import Glasses from './glasses'
-import { TweenMax, Power1 } from 'gsap/all';
-import ScrollMagic from 'scrollmagic';
-import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import Container from '../layout/container';
+import Glasses from './glasses';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import { Tween } from 'react-gsap';
+import { Controller, Scene } from 'react-scrollmagic';
 
 export default class About extends Component {
-
-  componentDidMount = () => {
-    this.props.controller.addScene(
-      new ScrollMagic.Scene({
-        duration: 0,
-        triggerElement: `.${styles.glasses}`,
-        offset: window.innerWidth < 768 ? 150 : 300,
-        reverse: false,
-        triggerHook: 1
-      })
-      .setTween(this.animatedGlasses())
-    );
-  }
-
-  animatedGlasses = () => {
-    let glasses = document.querySelectorAll('.glasses');
-    let glassesArr = [].slice.call(glasses);
-    let tweens = [];
-    glassesArr.forEach((element, i) => {
-      let value = 80;
-      if (i % 2 === 0) {
-        value = -value
-      }
-      tweens.push(TweenMax.from(element, 1, {autoAlpha: 0, x: Math.random()*value , y: Math.random()*value, yoyo:true, ease: Power1.easeOut}));
-      // TweenMax.fromTo(`.${styles.about}`, 3, {background: 'linear-gradient(to right, rgb(30, 41, 67), rgb(30, 41, 67))'}, {background: 'linear-gradient(to right, rgb(49, 110, 255), rgb(30, 41, 67))'});
-    });
-    return tweens;
-  }
-
   render() {
     return (
       <div className={styles.about} id={this.props.id || null}>
         <Container className={styles.container}>
           {/* <img src={glass} alt={'Vidros'} className={styles.glasses} /> */}
-          <Glasses className={styles.glasses}/>
+          <Controller>
+            <Scene
+              triggerElement={`.${styles.about}`}
+              reverse={false}
+              // offset={window.innerWidth < 768 ? 150 : 300}
+              triggerHook={1}
+              duration={120}
+            >
+              {progress => (
+                <Tween
+                  from={{
+                    autoAlpha: 0,
+                    x: Math.random() * 80,
+                    y: Math.random() * 80,
+                  }}
+                  ease="Power1"
+                  totalProgress={progress}
+                  paused
+                >
+                  <Glasses className={styles.glasses} />
+                </Tween>
+              )}
+            </Scene>
+          </Controller>
           <div className={styles.copy}>
-            <h3>Trabalhamos no segmento de Instalações Comerciais em Modulados de vidro temperado, principalmente no mercado de Salvador e interior da Bahia.</h3>
-            <h4>Fornecemos elaboração gratuita de projeto (perspectiva e planta baixa), e o nosso diferencial no mercado está na qualidade do nosso produto. Atuamos com diversos materiais, a exemplo de MDF mesclado com vidro temperado, uma parceria nossa com a <a href="http://prolojavidros.com.br/" target="_blank" rel="noopener noreferrer">PROLOJA</a>.</h4>
+            <h3>
+              Trabalhamos no segmento de Instalações Comerciais em Modulados de
+              vidro temperado, principalmente no mercado de Salvador e interior
+              da Bahia.
+            </h3>
+            <h4>
+              Fornecemos elaboração gratuita de projeto (perspectiva e planta
+              baixa), e o nosso diferencial no mercado está na qualidade do
+              nosso produto. Atuamos com diversos materiais, a exemplo de MDF
+              mesclado com vidro temperado, uma parceria nossa com a{' '}
+              <OutboundLink
+                href="http://prolojavidros.com.br/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                PROLOJA
+              </OutboundLink>
+              .
+            </h4>
           </div>
         </Container>
       </div>
-    )
+    );
   }
 }
