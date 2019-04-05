26ef8ba176043a8316351b4ab5406db82a342d0c
Helder Santos <contact@meendoo.com>
2019-04-04 23:04:27 -0300

    Fixed 'window is not defined' error on build

--- src/components/callout/callout.js
+++ src/components/callout/callout.js
@@ -1,45 +1,59 @@
-import React from 'react'
+import React from 'react';
 // import Container from '../layout/container'
 import styles from './callout.module.scss';
-import { TweenMax, Power1 } from 'gsap'
-import Tick from '../utils/tick'
-import ScrollMagic from 'scrollmagic';
-import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
-
+import { TweenMax, Power1 } from 'gsap';
+import Tick from '../utils/tick';
+if (typeof window !== `undefined`) {
+  var ScrollMagic = require('scrollmagic');
+  require('imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+}
 
 export default class callout extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.years = React.createRef();
     this.experience = React.createRef();
   }
   componentDidMount = () => {
     const years = {
-      value: 0
-    }
+      value: 0,
+    };
     const yearsSpan = this.years.current;
     const experienceSpan = this.experience.current;
     let tweens = [
-      TweenMax.from(experienceSpan, 2, {autoAlpha: 0.5}),
-      TweenMax.to(years, 2, {value: 15, ease: Power1.easeOut, roundProps: {value: 1}, onUpdate: ()=> {yearsSpan.innerHTML = String(years.value).padStart(2, '0')}})
-    ]
+      TweenMax.from(experienceSpan, 2, { autoAlpha: 0.5 }),
+      TweenMax.to(years, 2, {
+        value: 15,
+        ease: Power1.easeOut,
+        roundProps: { value: 1 },
+        onUpdate: () => {
+          yearsSpan.innerHTML = String(years.value).padStart(2, '0');
+        },
+      }),
+    ];
     this.props.controller.addScene(
       new ScrollMagic.Scene({
         duration: 0,
         triggerElement: `.${styles.title}`,
         reverse: false,
-        triggerHook: 1
-      })
-      .setTween(tweens)
+        triggerHook: 1,
+      }).setTween(tweens)
     );
-  }
-  
+  };
+
   render() {
     return (
       <div className={styles.callout}>
-        <h2 className={styles.title}>SÃO MAIS DE <em ref={this.experience}><span ref={this.years}>15</span> ANOS</em><br/>DE EXPERIÊNCIA</h2>
-        <Tick/>
+        <h2 className={styles.title}>
+          SÃO MAIS DE{' '}
+          <em ref={this.experience}>
+            <span ref={this.years}>15</span> ANOS
+          </em>
+          <br />
+          DE EXPERIÊNCIA
+        </h2>
+        <Tick />
       </div>
-    )
+    );
   }
 }
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,84 +1,118 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import 'react-photoswipe/lib/photoswipe.css';
-import { PhotoSwipeGallery} from 'react-photoswipe';
-import { Tick } from '../utils'
-import Container from '../layout/container'
-import './photoswipe-fix.scss'
-import styles from './gallery.module.scss'
-// import Button from '../button/'
-import ScrollMagic from 'scrollmagic';
-import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import { PhotoSwipeGallery } from 'react-photoswipe';
+import { Tick } from '../utils';
+import Container from '../layout/container';
+import './photoswipe-fix.scss';
+import styles from './gallery.module.scss';
+import Button from '../button/';
 import { TweenMax } from 'gsap/all';
+if (typeof window !== `undefined`) {
+  var ScrollMagic = require('scrollmagic');
+  require('imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
+}
 
 export default class Gallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isOpen: true,
-            imgsToShow: 10
-        }
-        this.options = {
-            history: false,
-            showHideOpacity: true,
-            getThumbBoundsFn: false,
-            bgOpacity: 0.95
-        }
-    }
-
-    handleClose = () => {
-        this.setState({isOpen: false});
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: true,
+      imgsToShow: 10,
     };
+    this.options = {
+      history: false,
+      showHideOpacity: true,
+      getThumbBoundsFn: false,
+      bgOpacity: 0.95,
+    };
+  }
 
-    getThumbnailContent = (item) => {
-        return (
-            <img className={styles.thumbnail} alt={'Imagem'} src={item.fluidSrc} width={200} height={200}/>
-        );
-    }
+  handleClose = () => {
+    this.setState({ isOpen: false });
+  };
 
-    showMore = () => {
-        this.setState({imgsToShow: this.state.imgsToShow + 10})
-    }
+  getThumbnailContent = item => {
+    return (
+      <img
+        className={styles.thumbnail}
+        alt={'Imagem'}
+        src={item.fluidSrc}
+        width={200}
+        height={200}
+      />
+    );
+  };
 
-    componentDidMount = () => {
-      this.props.controller.addScene( new ScrollMagic.Scene ({
+  showMore = () => {
+    this.setState({ imgsToShow: this.state.imgsToShow + 10 });
+  };
+
+  componentDidMount = () => {
+    this.props.controller.addScene(
+      new ScrollMagic.Scene({
         duration: 0,
         triggerElement: `.${styles.gallery}`,
         reverse: false,
-        triggerHook: 0.5
-      })
-        .setTween( TweenMax.staggerFrom(`.${styles.thumbnail}`, 0.3, {opacity: 0, y: 30}, 0.1 ))
-      );
-    }
-
-    componentDidUpdate = (prevProps, prevState) => {
-        let startPoint = prevState.imgsToShow;
-        let thumbs = Array.from(document.querySelectorAll(`.${styles.thumbnail}`));
-        let interval = thumbs.slice(startPoint);
-        TweenMax.staggerFrom(interval, 0.3, {opacity: 0, y: 30}, 0.05 )
-    }
-    
-    render() {
-        let items = [];
-        let edges = this.props.data.edges;
-        edges.slice(0, this.state.imgsToShow).forEach(img => {
-            let fluidSrc = img.node.childImageSharp.fluid.src;
-            let {src, width, height} = img.node.childImageSharp.original;
-            let item = {};
-            item.fluidSrc = fluidSrc;
-            item.src = src;
-            item.w = width;
-            item.h = height;
-            items.push(item);
-        });
-
-        return (
-            <div id={this.props.id || ''}>
-                <Container className={styles.gallery}>
-                    <h2>NOSSOS PROJETOS<Tick white/></h2>
-                    <PhotoSwipeGallery items={items} options={this.options} thumbnailContent={this.getThumbnailContent}/>
-                    {this.state.imgsToShow >= edges.length ? '' : <button onClick={this.showMore}>Ver Mais</button>}
-                </Container>
-            </div>
+        triggerHook: 0.5,
+      }).setTween(
+        TweenMax.staggerFrom(
+          `.${styles.thumbnail}`,
+          0.3,
+          { opacity: 0, y: 30 },
+          0.1
         )
-    }
+      )
+    );
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    let startPoint = prevState.imgsToShow;
+    let thumbs = Array.from(document.querySelectorAll(`.${styles.thumbnail}`));
+    let interval = thumbs.slice(startPoint);
+    TweenMax.staggerFrom(interval, 0.3, { opacity: 0, y: 30 }, 0.05);
+  };
+
+  render() {
+    let items = [];
+    let edges = this.props.data.edges;
+    edges.slice(0, this.state.imgsToShow).forEach(img => {
+      let fluidSrc = img.node.childImageSharp.fluid.src;
+      if (fluidSrc !== null) {
+        let { src, width, height } = img.node.childImageSharp.original;
+        let item = {};
+        item.fluidSrc = fluidSrc;
+        item.src = src;
+        item.w = width;
+        item.h = height;
+        items.push(item);
+      }
+    });
+
+    return (
+      <div id={this.props.id || ''}>
+        <Container className={styles.gallery}>
+          <h2>
+            NOSSOS PROJETOS
+            <Tick white />
+          </h2>
+          <PhotoSwipeGallery
+            items={items}
+            options={this.options}
+            thumbnailContent={this.getThumbnailContent}
+          />
+          {this.state.imgsToShow >= edges.length ? (
+            ''
+          ) : (
+            <Button
+              size={'md'}
+              className={styles.btnMore}
+              onClick={this.showMore}
+            >
+              Ver Mais
+            </Button>
+          )}
+        </Container>
+      </div>
+    );
+  }
 }
